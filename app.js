4e3aed33d3f95a9bd58d1a6105d7ed76151e62fd
Đinh Xuân Hùng <xuanhung312000@gmail.com>
2020-10-21 18:58:47 +0700

    up file

---
+++ app.js
@@ -0,0 +1,39 @@
+var str = '';
+
+function displayData(ch) {
+    if (ch == 'AC') {
+        str = '';
+        document.getElementById('display-main').innerHTML = 0;
+        document.getElementById('display-operations').innerHTML = 0;
+    } else if (ch == 'CE') {
+        str = str.substr(0, str.length - 1);
+        document.getElementById('display-operations').innerHTML = str;
+    } else if (ch == '=') {
+        ans = eval(str);
+        document.getElementById('display-main').innerHTML = ans;
+    } else if (ch == '%') {
+        str = eval(str) / 100;
+        document.getElementById('display-main').innerHTML = str;
+        document.getElementById('display-operations').innerHTML = str;
+    } else {
+        if (str == '') {
+            if (ch == '*' || ch == '/') {
+                str = str;
+            } else {
+                str += ch;
+            }
+
+        } else {
+            if (str.substr(-1) == '+' || str.substr(-1) == '-' || str.substr(-1) == '*' || str.substr(-1) == '/') {
+                if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                    str = str;
+                } else {
+                    str += ch;
+                }
+            } else {
+                str += ch;
+            }
+        }
+    }
+    document.getElementById('display-operations').innerHTML = str;
+}
